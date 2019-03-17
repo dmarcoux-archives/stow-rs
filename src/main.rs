94fd483c7a9177dbdd6edb93fafb2583a032e2f9
Dany Marcoux <danymarcoux@gmail.com>
2019-03-17 23:01:46 +0100

    Symlink the README.md
    I'm learning rust, so slowly heading to something useful

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::os::unix::fs;
+
+fn main() -> std::io::Result<()> {
+    symlink("README.md", "symlinked_README.md")
+}
+
+fn symlink(source: &str, destination: &str) -> std::io::Result<()> {
+    let current_dir = env::current_dir()?;
+    // TODO: Handle panic from .unwrap() and instead match
+    let target_dir = current_dir.parent().unwrap();
+
+    let source = current_dir.join(source);
+
+    let destination = target_dir.join(destination);
+
+    fs::symlink(source, destination)?;
+
+    Ok(())
 }
